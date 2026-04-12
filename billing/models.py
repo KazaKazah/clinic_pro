767f16b33ec34e94a804b52dced1a1bdcd5dbcb8
asan <elt.8155@gmail.com>
2026-04-12 20:54:52 +0500

    тест

--- billing/models.py
+++ billing/models.py
@@ -1,15 +1,17 @@
 from django.db import models
+from django.db import models
+from inventory.models import Material
+from billing.models import Service
+
 
 class Service(models.Model):
+    'сервис'
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return self.name
 
-from django.db import models
-from inventory.models import Material
-from billing.models import Service
 
 class ServiceMaterial(models.Model):
     service = models.ForeignKey(Service, on_delete=models.CASCADE)
